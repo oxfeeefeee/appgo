5edeccc939ed58217dd5f9aeb876dda35710d35a
oxfeeefeee <number5.ios@gmail.com>
2016-03-25 13:06:01 +0800

    fix token validation

--- auth/token.go
+++ auth/token.go
@@ -29,22 +29,26 @@ func NewToken(userId appgo.Id, role appgo.Role, lifetime int) Token {
 func (t Token) Validate() (appgo.Id, appgo.Role) {
 	byteToken, err := base64.StdEncoding.DecodeString(string(t))
 	if err != nil {
+		log.Infoln("validate token failed: ", err)
 		return 0, 0
 	}
 	key := appgo.Conf.RootKey
 	decrypted, err := crypto.Decrypt([]byte(byteToken), []byte(key))
 	if err != nil {
+		log.Infoln("validate token failed: ", err)
 		return 0, 0
 	}
 	subs := strings.Split(string(decrypted), ",")
 	if len(subs) != 3 {
+		log.Errorln("bad token format")
 		return 0, 0
 	}
-	userId := appgo.IdFromStr(subs[0])
+	userId := appgo.IdFromBase64(subs[0])
 	roleInt, _ := strconv.Atoi(subs[1])
-	expiry := time.Unix(0, int64(appgo.IdFromStr(subs[2])))
+	expiry := time.Unix(0, int64(appgo.IdFromBase64(subs[2])))
 	ttl := expiry.Sub(time.Now())
 	if ttl <= 0 {
+		log.Infoln("validate token failed: expired at ", expiry)
 		return 0, 0
 	}
 	return userId, appgo.Role(roleInt)
