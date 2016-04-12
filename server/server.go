75101543b948a177c7affe1db070e71eb2dfd4bd
oxfeeefeee <number5.ios@gmail.com>
2016-04-12 13:34:40 +0800

    add layout

--- server/server.go
+++ server/server.go
@@ -41,9 +41,10 @@ func (s *Server) AddRest(path string, rests []interface{}) {
 	}
 }
 
-func (s *Server) AddHtml(path string, htmls []interface{}, funcs template.FuncMap) {
+func (s *Server) AddHtml(path, layout string, htmls []interface{}, funcs template.FuncMap) {
 	renderer := render.New(render.Options{
 		Directory:     appgo.Conf.TemplatePath,
+		Layout:        layout,
 		Funcs:         []template.FuncMap{funcs},
 		IsDevelopment: appgo.Conf.DevMode,
 	})
