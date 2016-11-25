c974905241fa582894d7083f17e589363cc4e9e2
chen.fei <chenfei@wallstreetcn.com>
2016-11-25 16:47:17 +0800

    add redis lists

---
+++ redis/lists.go
@@ -0,0 +1,33 @@
+package redis
+
+import (
+	"fmt"
+)
+
+type Lists struct {
+	namespace string
+}
+
+func NewList(namespace string) *Lists {
+	return &Lists{namespace}
+}
+
+func (l *Lists) keystr(k interface{}) string {
+	return fmt.Sprintf("%s:%v", l.namespace, k)
+}
+
+func (l *Lists) Prepend(key interface{}, item interface{}) (int64, error) {
+	if newSize, err := Do("LPUSH", l.keystr(key), item); err != nil {
+		return 0, err
+	} else {
+		return newSize.(int64), nil
+	}
+}
+
+func (l *Lists) PopLastItem(key interface{}) (interface{}, error) {
+	if item, err := Do("RPOP", l.keystr(key)); err != nil {
+		return nil, err
+	} else {
+		return item, nil
+	}
+}
