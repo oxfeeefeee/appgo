90427d21d5213f0d9a395b71834b1274ea711beb
chen.fei <chenfei@wallstreetcn.com>
2016-11-28 14:02:45 +0800

    add AllItems func

--- redis/lists.go
+++ redis/lists.go
@@ -12,12 +12,20 @@ func NewLists(namespace string) *Lists {
 	return &Lists{namespace}
 }
 
-func (l *Lists) keystr(k interface{}) string {
+func (l *Lists) keyStr(k interface{}) string {
 	return fmt.Sprintf("%s:%v", l.namespace, k)
 }
 
+func (l *Lists) AllItems(k interface{}) ([]string, error) {
+	if items, err := Do("LRANGE", l.keyStr(k), 0, -1); err != nil {
+		return nil, err
+	} else {
+		return items.([]string), nil
+	}
+}
+
 func (l *Lists) Prepend(key interface{}, item interface{}) (int64, error) {
-	if newSize, err := Do("LPUSH", l.keystr(key), item); err != nil {
+	if newSize, err := Do("LPUSH", l.keyStr(key), item); err != nil {
 		return 0, err
 	} else {
 		return newSize.(int64), nil
@@ -25,7 +33,7 @@ func (l *Lists) Prepend(key interface{}, item interface{}) (int64, error) {
 }
 
 func (l *Lists) PopLastItem(key interface{}) (interface{}, error) {
-	if item, err := Do("RPOP", l.keystr(key)); err != nil {
+	if item, err := Do("RPOP", l.keyStr(key)); err != nil {
 		return nil, err
 	} else {
 		return item, nil
