0536c77c703c6c73834501e74a6756ac4a8b4d27
chen.fei <chenfei@wallstreetcn.com>
2017-03-09 18:39:40 +0800

    add Parse() method to auth.Token

--- auth/token.go
+++ auth/token.go
@@ -2,6 +2,8 @@ package auth
 
 import (
 	"encoding/base64"
+	"errors"
+	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"github.com/oxfeeefeee/appgo"
 	"github.com/oxfeeefeee/appgo/toolkit/crypto"
@@ -53,6 +55,26 @@ func (t Token) Validate() (appgo.Id, appgo.Role) {
 		return 0, 0
 	}
 	return userId, appgo.Role(roleInt)
+}
+
+func (t Token) Parse() (appgo.Id, appgo.Role, *time.Time, error) {
+	byteToken, err := base64.StdEncoding.DecodeString(string(t))
+	if err != nil {
+		return 0, 0, nil, errors.New(fmt.Sprintf("Parse(), DecodeString failed, err: %v", err))
+	}
+	key := appgo.Conf.RootKey
+	decrypted, err := crypto.Decrypt([]byte(byteToken), []byte(key))
+	if err != nil {
+		return 0, 0, nil, errors.New(fmt.Sprintf("Parse(), Decrypt failed, err: %v", err))
+	}
+	subs := strings.Split(string(decrypted), ",")
+	if len(subs) != 3 {
+		return 0, 0, nil, errors.New(fmt.Sprintf("Parse(), Split failed, bad token format"))
+	}
+	userId := appgo.IdFromBase64(subs[0])
+	roleInt, _ := strconv.Atoi(subs[1])
+	expiry := time.Unix(0, int64(appgo.IdFromBase64(subs[2])))
+	return userId, appgo.Role(roleInt), &expiry, nil
 }
 
 func tokenLifetime(role appgo.Role) int {
